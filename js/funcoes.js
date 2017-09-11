d474157e4a35942aba07212d199f03dcc25f82e0
Leonardo Lucas de Melo <leolucasm@edu.univali.br>
2017-09-11 14:39:37 -0300

    Criacao das paginas de contato e projetos

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,6 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -15,18 +14,15 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
     offset: 54
   });
 
-  // Collapse the navbar when page is scrolled
   $(window).scroll(function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
@@ -35,4 +31,4 @@
     }
   });
 
-})(jQuery); // End of use strict
+})(jQuery);
